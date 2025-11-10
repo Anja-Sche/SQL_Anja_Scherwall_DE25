caef12c2b0e5c5bf4453fbcbd1e6a3eb81f120a1
Anja Scherwall <anjagutt@hotmail.com>
2025-11-10 15:20:22 +0100

    03_sql_intro

---
+++ video_code_along/03_sql_intro/sql/queries.sql
@@ -0,0 +1,31 @@
+SELECT
+    *
+FROM
+    funny_jokes;
+
+-- shortcut for select * 
+FROM funny_jokes;
+
+/* ascending order */
+SELECT
+    *
+FROM
+    funny_jokes
+ORDER BY
+    rating;
+
+-- descending ordet
+SELECT
+    *
+FROM
+    funny_jokes
+ORDER BY
+    rating DESC;
+
+-- after updating joke id 7
+SELECT
+    *
+FROM
+    funny_jokes
+WHERE
+    id = 7;
---
+++ video_code_along/03_sql_intro/sql/update_joke.sql
@@ -0,0 +1,6 @@
+-- Updates rating of the joke to 10 iof the joke with id 7
+UPDATE funny_jokes
+SET
+    rating = 10
+WHERE 
+    id =7;
