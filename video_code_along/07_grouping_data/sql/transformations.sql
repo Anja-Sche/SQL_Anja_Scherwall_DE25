e0a32779572e86f1a8abba2be34fc41f707519d5
Anja Scherwall <anjagutt@hotmail.com>
2025-11-17 14:59:52 +0100

    code along 07

---
+++ video_code_along/07_grouping_data/sql/transformations.sql
@@ -0,0 +1,24 @@
+-- change name of id into food
+--remove google topic column
+-- change value int number_of_searches
+-- change week_id into week
+-- extract a year column
+
+SELECT
+    id AS food,
+    week_id AS week,
+    SUBSTRING(week, 1, 4) AS year,
+    value AS number_of_searches
+FROM
+    food;
+
+CREATE TABLE
+    IF NOT EXISTS cleaned_food AS (
+        SELECT 
+            id AS food,
+            week_id AS week,
+            SUBSTRING(week, 1, 4) AS year,
+            value AS number_of_searches
+        FROM
+    food
+    );
