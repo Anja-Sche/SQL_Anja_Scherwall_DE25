62d630081aa18038c7970fbd7bc0cb5566cd5ef5
Anja Scherwall <anjagutt@hotmail.com>
2025-11-17 12:17:57 +0100

    06 crud operations

---
+++ video_code_along/06_crud_operations/sql/3_crud_update.sql
@@ -0,0 +1,18 @@
+-- update operation using UPDATE statement
+-- checkmark for glossarys which we have learnt
+SELECT
+    *
+FROM
+    database.duckdb    
+WHERE
+    id IN (3, 6, 7);
+
+
+UPDATE
+    database.duckdb
+SET 
+    learnt = TRUE    
+WHERE
+    id IN (3, 6, 7);
+
+
