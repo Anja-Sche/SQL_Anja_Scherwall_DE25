c73cbd8c905748185ab7120b7826478c665236bb
Anja Scherwall <anjagutt@hotmail.com>
2025-11-20 12:36:24 +0100

    last of excercise 0

--- excercise/excercise_0/sql/1_exploring_hemnet_data.sql
+++ excercise/excercise_0/sql/1_exploring_hemnet_data.sql
@@ -57,3 +57,107 @@ WHERE
     ROUND(over_ten_milion / total_sales * 100,0) AS percentage_over_ten_milion
 FROM 
     staging.hemnet_data;
+
+
+
+-- ┌────────────────────────┬─────────────┬─────────┬─────────┬─────────┬─────────┐
+-- │      column_name       │ column_type │  null   │   key   │ default │  extra  │
+-- │        varchar         │   varchar   │ varchar │ varchar │ varchar │ varchar │
+-- ├────────────────────────┼─────────────┼─────────┼─────────┼─────────┼─────────┤
+-- │ column00               │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ address                │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ asked_price            │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ coordinate             │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ final_price            │ BIGINT      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ pourcentage_difference │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ land_area              │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ area                   │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ commune                │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ price_per_area         │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ rooms                  │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ sale_date              │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ supplemental_area      │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- ├────────────────────────┴─────────────┴─────────┴─────────┴─────────┴─────────┤
+-- │ 13 rows                                                            6 columns │
+-- └──────────────────────────────────────────────────────────────────────────────┘
+-- ┌──────────────┐
+-- │ count_star() │
+-- │    int64     │
+-- ├──────────────┤
+-- │     500      │
+-- └──────────────┘
+-- ┌────────────┐
+-- │ row_amount │
+-- │   int64    │
+-- ├────────────┤
+-- │    500     │
+-- └────────────┘
+-- ┌────────────────────────┬─────────────┬─────────┬─────────┬─────────┬─────────┐
+-- │      column_name       │ column_type │  null   │   key   │ default │  extra  │
+-- │        varchar         │   varchar   │ varchar │ varchar │ varchar │ varchar │
+-- ├────────────────────────┼─────────────┼─────────┼─────────┼─────────┼─────────┤
+-- │ column00               │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ address                │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ asked_price            │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ coordinate             │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ final_price            │ BIGINT      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ pourcentage_difference │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ land_area              │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ area                   │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ commune                │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ price_per_area         │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ rooms                  │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ sale_date              │ VARCHAR     │ YES     │ NULL    │ NULL    │ NULL    │
+-- │ supplemental_area      │ DOUBLE      │ YES     │ NULL    │ NULL    │ NULL    │
+-- ├────────────────────────┴─────────────┴─────────┴─────────┴─────────┴─────────┤
+-- │ 13 rows                                                            6 columns │
+-- └──────────────────────────────────────────────────────────────────────────────┘
+-- ┌──────────┬──────────────────────┬─────────────┬──────────────────────┬───┬────────────────┬────────┬─────────────────┬───────────────────┐
+-- │ column00 │       address        │ asked_price │      coordinate      │ . │ price_per_area │ rooms  │    sale_date    │ supplemental_area │
+-- │  double  │       varchar        │   double    │       varchar        │   │     double     │ double │     varchar     │      double       │
+-- ├──────────┼──────────────────────┼─────────────┼──────────────────────┼───┼────────────────┼────────┼─────────────────┼───────────────────┤
+-- │      9.8 │ Sångmöstigen 3       │  29900000.0 │ [59.32189, 17.94161] │ . │       129583.0 │    9.0 │ Såld 2022-03-27 │             111.0 │
+-- │      2.6 │ Vikavägen 15         │  28500000.0 │ [59.32079, 17.9391.  │ . │       118410.0 │    8.0 │ Såld 2022-04-17 │              20.0 │
+-- │     25.4 │ Gustaf Lundbergs v.  │  20500000.0 │ [59.33912277858042.  │ . │       131980.0 │   11.0 │ Såld 2022-05-10 │              96.0 │
+-- │     23.6 │ Bardabacken 17       │  26000000.0 │ [59.32154, 17.9376.  │ . │       113537.0 │   NULL │ Såld 2022-04-09 │             149.0 │
+-- │     24.3 │ Ferievägen 65        │  28000000.0 │ [59.33391840148430.  │ . │        66667.0 │   11.0 │ Såld 2022-05-19 │              24.0 │
+-- ├──────────┴──────────────────────┴─────────────┴──────────────────────┴───┴────────────────┴────────┴─────────────────┴───────────────────┤
+-- │ 5 rows                                                                                                              13 columns (8 shown) │
+-- └──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+-- ┌──────────┬─────────────────────┬─────────────┬──────────────────────┬───┬────────────────┬────────┬─────────────────┬───────────────────┐
+-- │ column00 │       address       │ asked_price │      coordinate      │ . │ price_per_area │ rooms  │    sale_date    │ supplemental_area │
+-- │  double  │       varchar       │   double    │       varchar        │   │     double     │ double │     varchar     │      double       │
+-- ├──────────┼─────────────────────┼─────────────┼──────────────────────┼───┼────────────────┼────────┼─────────────────┼───────────────────┤
+-- │     16.9 │ Småbrukarvägen 46C  │   1475000.0 │ [59.3704400502, 17.  │ . │        70952.0 │    2.0 │ Såld 2022-03-10 │              12.0 │
+-- │     19.4 │ Rymdvägen 13        │   3695000.0 │ [59.3821869, 17.83.  │ . │        38043.0 │    4.0 │ Såld 2022-05-11 │               0.0 │
+-- │     19.9 │ Själlandsgatan 85   │   3595000.0 │ [59.39785178704126.  │ . │        55263.0 │    3.0 │ Såld 2022-03-09 │               0.0 │
+-- │     37.2 │ Lammholmsbacken 66  │   4495000.0 │ [59.2705269, 17.89.  │ . │        41964.0 │    5.0 │ Såld 2022-05-27 │              19.0 │
+-- │     26.3 │ Lammholmsbacken 115 │   4490000.0 │ [59.2685051, 17.89.  │ . │        42478.0 │    5.0 │ Såld 2022-05-18 │              19.0 │
+-- ├──────────┴─────────────────────┴─────────────┴──────────────────────┴───┴────────────────┴────────┴─────────────────┴───────────────────┤
+-- │ 5 rows                                                                                                             13 columns (8 shown) │
+-- └─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+-- ┌────────────────┬─────────────────┬────────────────┬─────────────────┐
+-- │ minimum_price  │   mean_price    │  median_price  │  maximum_price  │
+-- │     int64      │     double      │     double     │      int64      │
+-- ├────────────────┼─────────────────┼────────────────┼─────────────────┤
+-- │    1490000     │   10047154.0    │   8852500.0    │    31100000     │
+-- │ (1.49 million) │ (10.05 million) │ (8.85 million) │ (31.10 million) │
+-- └────────────────┴─────────────────┴────────────────┴─────────────────┘
+-- ┌────────────────────┬─────────────────┬───────────────────┬────────────────────┐
+-- │ minimum_price_area │ mean_price_area │ median_price_area │ maximum_price_area │
+-- │       double       │     double      │      double       │       double       │
+-- ├────────────────────┼─────────────────┼───────────────────┼────────────────────┤
+-- │      20617.0       │     79120.0     │      71635.0      │      187500.0      │
+-- └────────────────────┴─────────────────┴───────────────────┴────────────────────┘
+-- ┌───────────────────┐
+-- │ distinct_communes │
+-- │       int64       │
+-- ├───────────────────┤
+-- │        12         │
+-- └───────────────────┘
+-- ┌─────────────┬─────────────────┬────────────────────────────┐
+-- │ total_sales │ over_ten_milion │ percentage_over_ten_milion │
+-- │    int64    │      int64      │           double           │
+-- ├─────────────┼─────────────────┼────────────────────────────┤
+-- │     500     │       173       │            35.0            │
+-- └─────────────┴─────────────────┴────────────────────────────┘
--- excercise/excercise_0/theory.md
+++ excercise/excercise_0/theory.md
@@ -4,27 +4,41 @@ These study questions are good to get an overview of how SQL and relational data
 
 &nbsp; a) What is SQL, and why is it important in relational databases?
 
-Structured Query Language is importatnt because it is a way to ???????
+Structured Query Language is a way to turn data in to valuble insights and handle inconsistent data. It can handle alot of data. 
 
 &nbsp; b) Explain the difference between FROM and WHERE clauses.
 
 FROM - the database where you get/have the information you are using for this code.
+
 WHERE - the conditions for the things you want to retreve. ex WHERE customer_id = 1. Get the information from the rows where customer_id is 1.
 
 &nbsp; c) Explain the purpose of the LIMIT or OFFSET clause in a SQL query.
 
 LIMIT - shows the amount of rows you ask for counting from the top. Ex LIMIT 4, shows the first 4 rows 
+
 OFFST - takes away the rows you ask forcounting from the top. Ex OFFSET 4, shows all rows except the first 4 rows.
 
 &nbsp; d) What are the primary functions of the Data Definition Language (DDL) in SQL?
 
+The primary functions is to create (tables, indexes, databases), alter ( change the structure of the existing table) and drop (permanently remove a databese object, table ...). 
+
 &nbsp; e) Explain the differences between the Data Manipulation Language (DML) and Data Definition Language (DDL).
+
+The difference between DDL and DML is that DDL is used to create, insert and drop(delete) for example tables or indexes. DML is used to insert, update and delete information/data inside the tables.
 
 &nbsp; f) What makes DuckDB unique compared to other databases?
 
+DuckDB stores the information i columns instead of in rows. 
+
 &nbsp; g) Is DuckDB suitable for real-time data processing?
 
+No.
+
 &nbsp; h) Difference between read_csv and read_csv_auto
+
+With read_csv the data types are put in by duckdb when it reads the file, also detects the column names automatically.
+
+With read_csv_auto you need to manually put in the data type for the columns and specify what the column names are.
 
 ## Glossary
 
@@ -32,30 +46,30 @@ Fill in this table either by copying this into your own markdown file or copy it
 
 | terminology         | explanation |
 | ------------------- | ----------- |
-| CRUD                |             |
-| query               |             |
-| statement           |             |
-| DDL                 |             |
-| DML                 |             |
-| idempotent          |             |
-| table               |             |
-| schema              |             |
-| row                 |             |
-| record              |             |
-| column              |             |
-| database            |             |
-| persistance         |             |
-| DQL                 |             |
-| relational database |             |
-| dot commands        |             |
-| CLI                 |             |
-| bash                |             |
-| type inference      |             |
-| clause              |             |
-| aliasing            |             |
-| SQL statement       |             |
-| persistent database |             |
-| .wal                |             |
-| IDE                 |             |
-| csv                 |             |
-| namespace           |             |
+| CRUD                |CREATE, READ, UPDATE, DELETE (creating and managing persistent data elements)            |
+| query               |computer programming language. Communicate and manipulate data in databases|
+| statement           |instructions (ex. select, )             |
+| DDL                 |Data Defenition Language (create, alter, drop)             |
+| DML                 |Data Defenition Language (insert, update, delete)             |
+| idempotent          |will give the same result everytime, as if it has only done it once (IF NOT EXISTS)             |
+| table               |database object composed of columns and rows to show the data            |
+| schema              |logicat grouping. like a folder where you have tables inside             |
+| row                 |a single group of related data within a table             |
+| record              |the rows are called records in database terminology             |
+| column              |the chategorries of information in a table             |
+| database            |organized collection of data             |
+| persistance         |the ways where processes or objects can continue on, or remain, even when systems are shut down             |
+| DQL                 |Data Query Language (select)             |
+| relational database |organizes data into columns and rows. Linked by common fields (keys)             |
+| dot commands        |a special built-in shell command             |
+| CLI                 |textbaserat gränssnitt. Interagera med datorn genom kommandon             |
+| bash                |used to write scripts and run commands             |
+| type inference      |the ability to automatically deduce, either partially or fully, the type of an expression at compile time             |
+| clause              |help fetch required records from database table             |
+| aliasing            |used to give a table, or a column, a temporary name             |
+| SQL statement       |a set of instructions             |
+| persistent database |the exact same data can be retrieved by an application when it's opened again             |
+| .wal                |Write-Ahead Logging (feature that ensures data integrity and durability.)             |
+| IDE                 |Integrated Development Enviroment             |
+| csv                 |dtata format fro storing tabular data where values are separated by commas             |
+| namespace           |defines a group of tables. Inside, all tables must be uniqley identified           |
