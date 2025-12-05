2a454d3887915781c708328f80d37251b00e960c
Anja Scherwall <anjagutt@hotmail.com>
2025-12-05 09:40:00 +0100

    updates

--- video_code_along/11_set_operations/sql/eda_sakila.sql
+++ video_code_along/11_set_operations/sql/eda_sakila.sql
@@ -28,3 +28,64 @@ WHERE
     c.first_name LIKE 'D%';
 
 -- more EDAs left for the reader
+
+
+--a)
+SELECT 
+    title,
+    length || ' minutes' AS length
+FROM film
+WHERE length > 180;
+
+
+--b)
+SELECT 
+    title,
+    rating,
+    length || ' minutes' AS length,
+    description
+FROM film
+WHERE title LIKE '%LOVE%';
+
+--olika fast samma resultat
+
+SELECT 
+    title,
+    rating,
+    length || ' minutes' AS length,
+    description
+FROM film
+WHERE 
+    regexp_matches(title, 'LOVE');
+
+
+--c)
+SELECT 
+    MIN(length) AS shortest_movie_min,
+    ROUND(AVG(length)) AS average_movie_length_min,
+    MEDIAN(length) AS median_movie_lentgh_min,
+    MAX(length) AS longes_movie_min
+FROM film;
+
+
+--d)
+SELECT
+    title,
+    rental_rate,
+    rental_duration,
+    ROUND(rental_rate/rental_duration, 2) AS rate_per_day
+FROM film
+ORDER BY rate_per_day DESC
+LIMIT 10;
+
+
+
+--e)
+SELECT
+    a.first_name || ' ' || a.last_name AS actor_name,
+    COUNT(DISTINCT fa.film_id) AS number_of_films
+FROM actor a
+    LEFT JOIN film_actor fa ON a.actor_id = fa.actor_id
+GROUP BY actor_name
+ORDER BY number_of_films DESC
+LIMIT 10;
